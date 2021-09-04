0702e8191e254e46c785ec001aa9894d4a82b36e
amirheesam2212 <76810809+amirheesam2212@users.noreply.github.com>
2021-09-04 23:30:08 +0430

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,137 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://sketchub.in/assets/styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- HTML Meta Tags -->
+    <title>Sketchub</title>
+    <meta name="description" content="Sketchub is a platform for sharing your programming creations with the world-wide community. It was first unofficial app created for sharing Sketchware Projects, but now you can share other programming projects too.">
+    <meta name="theme-color" content="#1A1A1A" />
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://sketchub.in">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Sketchelp">
+    <meta property="og:site_name" content="sketchub.in" />
+    <meta property="og:description" content="Sketchub is a platform for sharing your programming creations with the world-wide community. It was first unofficial app created for sharing Sketchware Projects, but now you can share other programming projects too.">
+    <meta property="og:image" content="https://sketchub.in/assets/images/thumbnail.png">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="ejemplo.me">
+    <meta property="twitter:url" content="https://sketchub.in">
+    <meta name="twitter:title" content="Sketchub">
+    <meta name="twitter:description" content="Sketchub is a platform for sharing your programming creations with the world-wide community. It was first unofficial app created for sharing Sketchware Projects, but now you can share other programming projects too.">
+    <meta name="twitter:image" content="https://sketchub.in/assets/images/thumbnail.png">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img src="https://sketchub.in/assets/images/Sketchub_Favicon.svg" class="logo">
+            <div class="navbtn">
+                <span>MENU</span>
+                <span>&#9776;</span>
+            </div>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#features">Features</a>
+                <a href="#social">Community</a>
+                <a href="https://forum.sketchub.in">Forums</a>
+                <a href="https://blog.sketchub.in">Blog</a>
+                <a href="#social">Contact</a>
+            </nav>
+        </div>
+    </header>
+    <div class="content">
+        <div class="home">
+            <div class="container">
+                <div class="mockups">
+                    <div class="desktop-mockup left">
+                        <img src="https://sketchub.in/assets/images/desktop.homepage.png">
+                    </div>
+                    <div class="phone-mockup left">
+                        <img src="https://sketchub.in/assets/images/screenshots/homepage.jpg">
+                    </div>
+                </div>
+                <div>
+                    <h1>A Better Platform For Sharing Your <b>Awesome Creations</b></h1>
+                    <p>Share your amazing stuff through Sketchub.</p>  <!-- It's a bit empty lol -->
+                    <div class="buttons">
+                        <button onclick="location.href='https://sketchub.in/latest.apk';">Download APK (v3.0)</button>
+                        <button onclick="location.href='https://web.sketchub.in';">Open in browser</button><br>
+                        <!--class="skweb" to hide the button for mobile view-->
+                    </div>
+                    <br>
+                    <a href="https://play.google.com/store/apps/details?id=in.sketchub.app" target="_blank"><img class="gpbutton" src="assets/images/en_badge_web_generic.png" alt="Download it on Google Play"></a>
+                </div>
+            </div>
+        </div>
+        <div class="features" id="features">
+            <div>
+                <div class="container">
+                    <div class="text">
+                        <h1>+10k Users</h1>
+                        <p>Join the community of over 10k members</p>
+                    </div>
+                    <div class="phone-mockup">
+                        <img src="https://sketchub.in/assets/images/screenshots/homepage.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="grey">
+                <div class="container">
+                    <div class="text">
+                        <h1>~3000 Projects</h1>
+                        <p>Browse thousands of quality projects directly shared by Developers</p>
+                    </div>
+                    <div class="phone-mockup">
+                        <img src="https://sketchub.in/assets/images/screenshots/project_page.jpg">
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="container">
+                    <div class="text">
+                        <h1>Feedback</h1>
+                        <p>Have a nice communication with community through advanced comments</p>
+                    </div>
+                    <div class="phone-mockup">
+                        <img src="https://sketchub.in/assets/images/screenshots/comments.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="grey">
+                <div class="container">
+                    <div class="text">
+                        <h1>Custom themes</h1>
+                        <p>Organise the look of app according to your taste</p>
+                    </div>
+                    <div class="phone-mockup">
+                        <img src="https://sketchub.in/assets/images/screenshots/theme.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="community" id="social">
+            <div class="container">
+                <h1>We're on <a href="https://discord.gg/wEathsbhXc">Discord</a>, <a href="https://t.me/sketchubt">Telegram</a> and <a href="https://www.facebook.com/sketchub.sw">Facebook</a>!</h1>
+                <p><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="284b47465c494b5c685b434d5c4b405d4a064146">[email&#160;protected]</a></p>
+                <!--<p>We also have a forum at <a href="http://forum.sketchub.in/">forum.sketchub.in.</a></p>-->
+            </div>
+        </div>
+        <div class="princing" id="price"></div>
+        <div class="footer">
+            <p>Google Play and the Google Play logo are trademarks of Google LLC.</p>
+        </div>
+    </div>
+    <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/scripts/main.js"></script>
+</body>
+</html>
+
+<!-- Legal text to put in footer later :
+
+    Google Play and the Google Play logo are trademarks of Google LLC.
+-->
